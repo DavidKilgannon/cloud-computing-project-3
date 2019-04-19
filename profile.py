8f1d101c66ecb5cff586b86f3ffaf8260f134feb
DavidKilgannon <43589348+DavidKilgannon@users.noreply.github.com>
2019-04-19 13:19:25 -0400

    Commented Code
    Commented code from cluster-template that allows for more nodes and centos7

--- profile.py
+++ profile.py
@@ -19,6 +19,28 @@ tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
+#code from cluster-template, if we want to use CENTOS7 instead AND have additional nodes.
+#prefixForIP = "192.168.1."
+#link = request.LAN("lan")
+#for i in range(6):
+# if i == 0:
+#   node = request.XenVM("head")
+#   node.routable_control_ip = "true"
+# elif i == 1:
+#   node = request.XenVM("metadata")
+# elif i == 2:
+#   node = request.XenVM("storage")
+# else:
+#   node = request.XenVM("compute-" + str(i-2))
+#   node.cores = 4
+#   node.ram = 4096
+    
+# node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  
+# iface = node.addInterface("if" + str(i))
+#  iface.component_id = "eth1"
+#  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+#link.addInterface(iface)
 # Create a XenVM
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
