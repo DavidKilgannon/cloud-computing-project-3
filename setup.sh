55149b643c211fb2b133586075434625694db708
DavidKilgannon <43589348+DavidKilgannon@users.noreply.github.com>
2019-04-19 15:15:36 -0400

    Docker repo install
    Install docker from repo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
 set -x
 sudo apt -y update
 
+#setup docker
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 #setup apache
 sudo apt install -y apache2
 sudo ufw allow in "Apache Full"
